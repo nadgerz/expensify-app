da04cd83566c91077268e2c09e1bf3e7a10a190d
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-26 17:49:22 +0200

    Make them line up!

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ const name = 'Steve';
 
 console.log( `${name}, app.js is running` );
 
-const template = (<div><h1>Indecision App</h1><p>This is some info</p></div>);
+const template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+  </div>
+);
 
 var appRoot = document.getElementById( 'app' );
 
