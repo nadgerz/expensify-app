af10b7c95b5bc93ba0966019445924560f1c5502
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-26 13:28:46 +0200

    Hmm problems...

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,7 @@
 var template = 'Steve';
 
 console.log( `${template}, app.js is running` );
+
+template = <p>This is JSX from app.js</p>;
+
+ReactDom.render( template, document.getElementById( 'id' ) );
