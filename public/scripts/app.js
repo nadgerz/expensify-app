c87ac8c4d9944876848e24bad8d45cb18ac4e3cb
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 13:48:34 +0200

    challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var square = function square(x) {
   return x * x;
@@ -20,3 +20,16 @@ console.log(square(8));
 console.log(namedFunc(7));
 console.log(squareArrow(5));
 console.log(tripleArrow(3));
+
+var getFirstName1 = function getFirstName1(name) {
+  return name.split(' ')[0];
+};
+
+var getFirstName2 = function getFirstName2(name) {
+  return name.split(' ')[0];
+};
+
+var name = 'Steve Ingram';
+
+console.log(getFirstName1(name));
+console.log(getFirstName2(name));
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -16,3 +16,14 @@ console.log( square( 8 ) );
 console.log( namedFunc( 7 ) );
 console.log( squareArrow( 5 ) );
 console.log( tripleArrow( 3 ) );
+
+const getFirstName1 = ( name ) => {
+  return name.split( ' ' )[0];
+};
+
+const getFirstName2 = ( name ) => name.split( ' ' )[0];
+
+const name = 'Steve Ingram';
+
+console.log( getFirstName1( name ) );
+console.log( getFirstName2( name ) );
