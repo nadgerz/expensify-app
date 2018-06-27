973cab3659209a6e2b5557488103d20dc4e72678
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 13:37:19 +0200

    arrow function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+var square = function square(x) {
+  return x * x;
+};
+
+var squareArrow = function squareArrow(x) {
+  return x * x;
+};
+
+console.log(square(8));
+console.log(squareArrow(5));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,10 @@
+const square = function( x ) {
+  return x * x;
+};
+
+const squareArrow = ( x ) => {
+  return x * x;
+};
+
+console.log( square( 8 ) );
+console.log( squareArrow( 5 ) );
