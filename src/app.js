1f79a4bef3d4bde1e4b7f5d69f0302014a993187
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 16:48:34 +0200

    Interesting

--- src/app.js
+++ src/app.js
@@ -26,27 +26,39 @@ const template = (
 let count = 0;
 
 const addOne = () => {
-  console.log( '+1' );
+  count++;
+  renderCounterApp();
+  console.log( '+1', count );
 };
 
 const minusOne = () => {
+  count--;
+  renderCounterApp();
   console.log( '-1' );
 };
 
 const reset = () => {
+  count = 0;
+  renderCounterApp();
   console.log( 'reset to 0' );
 };
 
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    {/*<button onClick={() => console.log( 'KMA!' )}>+1</button>*/}
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
-
 const appRoot = document.getElementById( 'app' );
 
-ReactDOM.render( templateTwo, appRoot );
+const renderCounterApp = () => {
+  
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      {/*<button onClick={() => console.log( 'KMA!' )}>+1</button>*/}
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+  
+  ReactDOM.render( templateTwo, appRoot );
+  
+};
+
+renderCounterApp();
