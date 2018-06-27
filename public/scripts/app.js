5ab8d1fb8872e0dcae9f967d5850d4ca4204ef4e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 19:17:11 +0200

    WORKING!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,7 +9,7 @@ console.log(name + ', app.js is running');
 var app = {
   title: 'My Big App',
   subTitle: 'This is more info',
-  options: ['One', 'Two']
+  options: []
 };
 
 var handleOnFormSubmit = function handleOnFormSubmit(event) {
@@ -25,53 +25,65 @@ var handleOnFormSubmit = function handleOnFormSubmit(event) {
     app.options.push(option);
 
     event.target.elements.option.value = '';
+
+    renderApp();
   }
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    'p',
-    null,
-    app.subTitle
-  ),
-  React.createElement(
-    'h3',
-    null,
-    app.options && app.options.length > 0 ? 'Here are your options:'.toLocaleUpperCase() : 'No soup for you!'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item One'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item Two'
-    )
-  ),
-  React.createElement(
-    'form',
-    { onSubmit: handleOnFormSubmit },
-    React.createElement('input', { type: 'text', name: 'option' }),
-    React.createElement(
-      'button',
-      null,
-      'Add Option'
-    )
-  )
-);
-
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderApp = function renderApp() {
+
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subTitle && React.createElement(
+      'p',
+      null,
+      app.subTitle
+    ),
+    React.createElement(
+      'h3',
+      null,
+      app.options && app.options.length > 0 ? 'Here are your options:'.toLocaleUpperCase() : 'No soup for you!'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item One'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item Two'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: handleOnFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ console.log( `${name}, app.js is running` );
 const app = {
   title: 'My Big App',
   subTitle: 'This is more info',
-  options: ['One', 'Two']
+  options: []
 };
 
 const handleOnFormSubmit = ( event ) => {
@@ -23,27 +23,36 @@ const handleOnFormSubmit = ( event ) => {
     app.options.push( option );
     
     event.target.elements.option.value = '';
+    
+    renderApp();
   }
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    <h3>{(app.options && app.options.length > 0) ? 'Here are your options:'.toLocaleUpperCase() : 'No soup for you!'}</h3>
-    
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-    </ol>
-    
-    <form onSubmit={handleOnFormSubmit}>
-      <input type="text" name="option"/>
-      <button>Add Option</button>
-    </form>
-  </div>
-);
-
 const appRoot = document.getElementById( 'app' );
 
-ReactDOM.render( template, appRoot );
+const renderApp = () => {
+  
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <h3>{(app.options && app.options.length > 0) ? 'Here are your options:'.toLocaleUpperCase() : 'No soup for you!'}</h3>
+      
+      <p>{app.options.length}</p>
+      
+      <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+      </ol>
+      
+      <form onSubmit={handleOnFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  
+  ReactDOM.render( template, appRoot );
+};
+
+renderApp();
