bf95edb38ef93599a3808c2860e6bc0297bcd302
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 13:10:57 +0200

    now using let

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,9 @@ var nameVar = 'Steve';
 console.log('nameVar', nameVar);
 var nameVar = 'Mike';
 console.log('nameVar', nameVar);
+
+var nameLet = 'Jeff';
+console.log('nameLet', nameLet);
+
+nameLet = 'Julie';
+console.log('nameLet', nameLet);
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -2,3 +2,9 @@ var nameVar = 'Steve';
 console.log( 'nameVar', nameVar );
 var nameVar = 'Mike';
 console.log( 'nameVar', nameVar );
+
+let nameLet = 'Jeff';
+console.log( 'nameLet', nameLet );
+
+nameLet = 'Julie';
+console.log( 'nameLet', nameLet );
