2cf8454120b522152908f36e4287075a98539f15
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 13:23:57 +0200

    Block scoping

---
+++ public/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var nameVar = 'Steve';
+console.log('nameVar', nameVar);
+var nameVar = 'Mike';
+console.log('nameVar', nameVar);
+
+var nameLet = 'Jeff';
+console.log('nameLet', nameLet);
+
+nameLet = 'Julie';
+console.log('nameLet', nameLet);
+
+var nameConst = 'Frank';
+console.log('nameConst', nameConst);
+
+function getPetName() {
+  var petName = 'Hal';
+  return petName;
+}
+
+var petName = getPetName();
+console.log(petName);
+
+// Block scoping
+
+var fullName = 'Steve ingram';
+
+if (fullName) {
+  var firstName = fullName.split(' ')[0];
+  console.log(firstName);
+}
+
+firstName = 'Doink';
+console.log(firstName);
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -11,3 +11,23 @@ console.log( 'nameLet', nameLet );
 
 const nameConst = 'Frank';
 console.log( 'nameConst', nameConst );
+
+function getPetName() {
+  var petName = 'Hal';
+  return petName;
+}
+
+const petName = getPetName();
+console.log( petName );
+
+// Block scoping
+
+var fullName = 'Steve ingram';
+
+if ( fullName ) {
+  var firstName = fullName.split( ' ' )[0];
+  console.log( firstName );
+}
+
+firstName = 'Doink';
+console.log( firstName );
