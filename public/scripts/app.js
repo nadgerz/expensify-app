753cfd05c4c411b8919a1ac12be4efb0ea071223
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 16:58:33 +0200

    Save stuff aside

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,78 +4,46 @@ var name = 'Steve';
 
 console.log(name + ', app.js is running');
 
-/*
-const app = {
+var app = {
   title: 'My Big App',
   subTitle: 'This is more info',
   options: ['One', 'Two']
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    <h3>{(app.options && app.options.length > 0) ? 'Here are your options:'.toLocaleUpperCase() : 'No soup for you!'}</h3>
-    
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-    </ol>
-  </div>
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subTitle && React.createElement(
+    'p',
+    null,
+    app.subTitle
+  ),
+  React.createElement(
+    'h3',
+    null,
+    app.options && app.options.length > 0 ? 'Here are your options:'.toLocaleUpperCase() : 'No soup for you!'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item Two'
+    )
+  )
 );
-*/
-
-var count = 0;
-
-var addOne = function addOne() {
-  count++;
-  renderCounterApp();
-  console.log('+1', count);
-};
-
-var minusOne = function minusOne() {
-  count--;
-  renderCounterApp();
-  console.log('-1');
-};
-
-var reset = function reset() {
-  count = 0;
-  renderCounterApp();
-  console.log('reset to 0');
-};
 
 var appRoot = document.getElementById('app');
 
-var renderCounterApp = function renderCounterApp() {
-
-  var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Count: ',
-      count
-    ),
-    React.createElement(
-      'button',
-      { onClick: addOne },
-      '+1'
-    ),
-    React.createElement(
-      'button',
-      { onClick: minusOne },
-      '-1'
-    ),
-    React.createElement(
-      'button',
-      { onClick: reset },
-      'reset'
-    )
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const name = 'Steve';
 
 console.log( `${name}, app.js is running` );
 
-/*
 const app = {
   title: 'My Big App',
   subTitle: 'This is more info',
@@ -21,44 +20,7 @@ const template = (
     </ol>
   </div>
 );
-*/
-
-let count = 0;
-
-const addOne = () => {
-  count++;
-  renderCounterApp();
-  console.log( '+1', count );
-};
-
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-  console.log( '-1' );
-};
-
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-  console.log( 'reset to 0' );
-};
 
 const appRoot = document.getElementById( 'app' );
 
-const renderCounterApp = () => {
-  
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      {/*<button onClick={() => console.log( 'KMA!' )}>+1</button>*/}
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
-  
-  ReactDOM.render( templateTwo, appRoot );
-  
-};
-
-renderCounterApp();
+ReactDOM.render( template, appRoot );
