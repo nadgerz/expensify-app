8d351a3106f9fc27235d34ce0d6973df289430ac
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-27 13:38:55 +0200

    ** booyah!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,5 +8,10 @@ var squareArrow = function squareArrow(x) {
   return x * x;
 };
 
+var tripleArrow = function tripleArrow(x) {
+  return Math.pow(x, 3);
+};
+
 console.log(square(8));
 console.log(squareArrow(5));
+console.log(tripleArrow(3));
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -6,5 +6,8 @@ const squareArrow = ( x ) => {
   return x * x;
 };
 
+const tripleArrow = ( x ) => x ** 3;
+
 console.log( square( 8 ) );
 console.log( squareArrow( 5 ) );
+console.log( tripleArrow( 3 ) );
