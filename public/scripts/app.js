a18ef561132ad0edf5154b191ab32dcd345d3c8e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-28 12:23:52 +0200

    ES6 defaults

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,14 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function Person(name) {
+var Person = function Person() {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'testy';
+
   _classCallCheck(this, Person);
 
   // console.log( 'test' );
   // console.log( name );
-  this.name = name || 'test';
+  this.name = name;
 };
 
 var me = new Person('Steve Ingram');
