9c54cbeea2f893824f13b60976f5e9d3b63faff8
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-28 12:15:40 +0200

    start of a simple class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,10 @@
-'use strict';
+"use strict";
 
-console.clear();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var defaultButtonText = 'Show';
-
-var app = {
-  visible: false,
-  buttonText: defaultButtonText
+var Person = function Person() {
+  _classCallCheck(this, Person);
 };
 
-var toggleDetails = function toggleDetails() {
-  app.visible = !app.visible;
-
-  app.buttonText = defaultButtonText;
-
-  if (app.visible) {
-    app.buttonText = 'Hide';
-  }
-
-  renderApp();
-};
-
-var appRoot = document.getElementById('app');
-
-var renderApp = function renderApp() {
-
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Visibility Toggle'
-    ),
-    React.createElement(
-      'button',
-      { onClick: toggleDetails },
-      app.buttonText,
-      ' Details'
-    ),
-    app.visible && React.createElement(
-      'p',
-      null,
-      'Big Boobs!'
-    )
-  );
-
-  ReactDOM.render(template, appRoot);
-};
-
-renderApp();
+var me = new Person();
+console.log(me);
