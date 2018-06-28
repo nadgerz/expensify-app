b89a7510b65480e2fcc59739face2f4a03ce7ce7
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-28 13:46:52 +0200

    Now have our wrapper element

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+class IndecisionApp extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <Header/>
+        
+        <Action/>
+        
+        <Options/>
+        
+        <AddOption/>
+      </div>
+    );
+  }
+}
+
+
 class Header extends React.Component {
   
   render() {
@@ -49,15 +67,7 @@ class AddOption extends React.Component {
 
 const jsx = (
   <div>
-    
-    <Header/>
-    
-    <Action/>
-    
-    <Options/>
-    
-    <AddOption/>
-  
+    <IndecisionApp/>
   </div>
 );
 
