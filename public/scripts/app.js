c471a408690643cde6a5c11689c004aeed74bef7
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-28 16:35:28 +0200

    Remember this style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,6 +19,10 @@ var Counter = function (_React$Component) {
     _this.handleAddOne = _this.handleAddOne.bind(_this);
     _this.handleMinusOne = _this.handleMinusOne.bind(_this);
     _this.handleReset = _this.handleReset.bind(_this);
+
+    _this.state = {
+      count: 0
+    };
     return _this;
   }
 
@@ -26,16 +30,31 @@ var Counter = function (_React$Component) {
     key: 'handleAddOne',
     value: function handleAddOne() {
       console.log('handleAddOne');
+      this.setState(function (prevState, props) {
+        return {
+          count: prevState.count + 1
+        };
+      });
     }
   }, {
     key: 'handleMinusOne',
     value: function handleMinusOne() {
       console.log('handleMinusOne');
+      this.setState(function (prevState, props) {
+        return {
+          count: prevState.count - 1
+        };
+      });
     }
   }, {
     key: 'handleReset',
     value: function handleReset() {
       console.log('handleReset');
+      this.setState(function (prevState, props) {
+        return {
+          count: 0
+        };
+      });
     }
   }, {
     key: 'render',
@@ -46,7 +65,8 @@ var Counter = function (_React$Component) {
         React.createElement(
           'h1',
           null,
-          'Count: '
+          'Count: ',
+          this.state.count
         ),
         React.createElement(
           'button',
