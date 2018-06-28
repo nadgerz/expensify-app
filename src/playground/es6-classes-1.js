a2f0abe33a2dddca590b19469916febea435c351
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-28 12:50:45 +0200

    undefined, not null

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,16 +1,27 @@
 class Person {
-  constructor( name = 'Anonymous' ) {
+  constructor( name = 'Anonymous', age = 0 ) {
     this.name = name;
+    this.age = age;
   }
   
   getGreeting() {
     return `Hi. I am ${this.name}!`;
   }
+  
+  getDescription() {
+    return `${this.name} is ${this.age} years old.`;
+  }
 }
 
 
-const me = new Person( 'Steve Ingram' );
+const me = new Person( 'Steve Ingram', 52 );
 console.log( me.getGreeting() );
+console.log( me.getDescription() );
 
 const other = new Person();
 console.log( other.getGreeting() );
+console.log( other.getDescription() );
+
+const boop = new Person( undefined, 18);
+console.log( boop.getGreeting() );
+console.log( boop.getDescription() );
