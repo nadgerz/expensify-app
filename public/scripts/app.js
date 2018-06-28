2630be6dc77b31568be56790cbcf248e1cf8817f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-28 12:19:10 +0200

    ES6 method declaration

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,13 @@
-"use strict";
+'use strict';
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function Person() {
+var Person = function Person(name) {
   _classCallCheck(this, Person);
+
+  console.log('test');
+  console.log(name);
 };
 
-var me = new Person();
+var me = new Person('Steve Ingram');
 console.log(me);
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,6 +1,10 @@
 class Person {
+  constructor( name ) {
+    console.log( 'test' );
+    console.log( name );
+  }
 }
 
 
-const me = new Person();
+const me = new Person( 'Steve Ingram' );
 console.log( me );
