f7f0837bbd22519649bba62ba4695dc3aca53bb8
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-29 20:11:04 +0200

    different way to import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 
-import validator from 'validator';
+// import validator from 'validator';
+import isEmail from 'validator/lib/isEmail';
 
 
-console.log( validator.isEmail( 'stephen.a.ingram@gmail.com' ) );
+// console.log( validator.isEmail( 'stephen.a.ingram@gmail.com' ) );
+console.log( isEmail( 'stephen.a.ingram@gmail.com' ) );
