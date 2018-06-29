7103061ccb38309adb32b0e880ac750b777d39fc
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-29 19:12:43 +0200

    not sure which one I prefer

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 console.log( 'utils.js is running!' );
 
-const square = ( x ) => x * x;
-const add = ( a, b ) => a + b;
+export const square = ( x ) => x * x;
+export const add = ( a, b ) => a + b;
 
-export { square, add };
+// export { square, add };
