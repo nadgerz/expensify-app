b7bc2e55b39aa8a44c596a73f41bfba3faf2bb4e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-29 18:39:28 +0200

    Added utils.js and import

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 console.clear();
+
+import './utils';
+
 console.log( 'app.js is running!' );
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,6 @@ module.exports = {
   output: {
     path: path.resolve( __dirname, 'public' ),
     filename: 'bundle.js'
-  }
+  },
+  mode: 'development'
 };
