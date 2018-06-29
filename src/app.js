2cfdec1989658c3ef72cf779e5a0fe5452c30ae2
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-29 20:16:01 +0200

    But babel is dead at the moment

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import validator from 'validator';
 import isEmail from 'validator/lib/isEmail';
 
-
 // console.log( validator.isEmail( 'stephen.a.ingram@gmail.com' ) );
 console.log( isEmail( 'stephen.a.ingram@gmail.com' ) );
+
+const template = <p>testing 123</p>;
+
+ReactDOM.render( template, document.getElementById( 'add' ) );
