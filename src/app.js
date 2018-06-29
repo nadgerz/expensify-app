ca1c591f40a4db292ee0831d0da25cfc513c1351
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-29 20:06:36 +0200

    yarn add validator

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-import anything, { add, square } from './utils';
-import isOldFart, { canDrink, isAdult } from './person';
+
+import validator from 'validator';
 
 
-console.clear();
-console.log( 'app.js is running!' );
-
-console.log( square( 4 ) );
-console.log( add( 4, 23 ) );
-console.log( anything( 9, 23 ) );
-
-console.log( canDrink( 4 ) );
-console.log( isAdult( 23 ) );
-console.log( isOldFart( 66 ) );
+console.log( validator.isEmail( 'stephen.a.ingram@gmail.com' ) );
