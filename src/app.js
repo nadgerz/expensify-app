412367074a299028900ee2ad62479e5b5fd37db7
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-29 19:34:49 +0200

    not sure which one I prefer

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { add, square } from './utils';
+import { canDrink, isAdult } from './person';
 
 
 console.clear();
@@ -6,3 +7,7 @@ console.log( 'app.js is running!' );
 
 console.log( square( 4 ) );
 console.log( add( 4, 23 ) );
+
+console.log( canDrink( 4 ) );
+console.log( isAdult( 23 ) );
+
