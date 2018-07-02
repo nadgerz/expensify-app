5f25b071c78c377ddaf2b99117ab070daeea72f0
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-02 18:11:21 +0200

    subtitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Header = ( props ) => (
   <div className={'header'}>
     
-    <h1>{props.title}</h1>
+    <h1 className={'header__title'}>{props.title}</h1>
     {
-      props.subTitle && <h2>{props.subTitle}</h2>
+      props.subTitle && <h2 className={'header__subtitle'}>{props.subTitle}</h2>
     }
   
   </div>
