dbfe4d7fae9b523b20b1e760613cfb38f4d18477
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-02 21:22:46 +0200

    CIAO

--- src/playground/person.js
+++
@@ -1,8 +0,0 @@
-console.log( 'person.js is running!' );
-
-const isAdult = ( age ) => age >= 18;
-const canDrink = ( age ) => age >= 16;
-
-export { isAdult, canDrink };
-
-export default ( age ) => age >= 65;
