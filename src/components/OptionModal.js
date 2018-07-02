b6e961436fdc9b7eae08a5539952991791764e3e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-02 13:11:14 +0200

    stateless

---
+++ src/components/OptionModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Modal from 'react-modal'
+
+const OptionModal = ( props ) => {
+    console.log( props );
+    
+    return (
+      <div>
+        some modal text
+      </div>
+    )
+};
+
+export default OptionModal;
