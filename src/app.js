375b9cb67429eb5e5f1dff73c65e5c40b6cc9a44
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-02 14:59:59 +0200

    CODE RED!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+
+import './styles/styles.css';
 /*
 import validator from 'validator';
 import isEmail from 'validator/lib/isEmail';
