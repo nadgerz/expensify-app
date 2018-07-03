48f0ef5d9ecbb1f16e332fd741da07ffb730bf70
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-03 19:53:32 +0200

    works, but indentation... hmm....

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,15 +3,30 @@ import { createStore } from 'redux';
 
 console.clear();
 
-const store = createStore( ( state = { count: 0 } ) => {
-  return state;
-} );
+const store = createStore( ( state = { count: 0 }, action ) => {
+                             console.log( 'running' );
+  
+                             if ( action.type === 'INCREMENT' ) {
+                               return {
+                                 count: state.count + 1
+                               };
+                             }
+                             else {
+                               return state;
+                             }
+                           }
+);
 
 console.log( store.getState() );
 
 // Actions - an object that gets sent to the store
 
 // I'd like to increment the count
+store.dispatch(
+  {
+    type: 'INCREMENT'
+  } );
+
 // I'd like to reset the count to zero
 
 console.log( store.getState() );
