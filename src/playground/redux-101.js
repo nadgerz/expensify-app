2fedd062498395217fc85428cb0066556bd39df5
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-03 19:47:06 +0200

    Actions...

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -9,6 +9,9 @@ const store = createStore( ( state = { count: 0 } ) => {
 
 console.log( store.getState() );
 
+// Actions - an object that gets sent to the store
+
 // I'd like to increment the count
 // I'd like to reset the count to zero
 
+console.log( store.getState() );
