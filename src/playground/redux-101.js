d5ec05ac9505334220ecd0d304af33f91a328636
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-03 19:55:57 +0200

    No error, just returns state as the default

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -6,6 +6,14 @@ console.clear();
 const store = createStore( ( state = { count: 0 }, action ) => {
                              console.log( 'running' );
   
+                             switch ( action.type ) {
+                               case 'INCREMENT':
+                                 return state;
+    
+                               default:
+                                 return state;
+                             }
+  
                              if ( action.type === 'INCREMENT' ) {
                                return {
                                  count: state.count + 1
@@ -22,10 +30,17 @@ console.log( store.getState() );
 // Actions - an object that gets sent to the store
 
 // I'd like to increment the count
-store.dispatch(
-  {
-    type: 'INCREMENT'
-  } );
+store.dispatch( {
+                  type: 'INCREMENT'
+                } );
+
+store.dispatch( {
+                  type: 'INCREMENT'
+                } );
+
+store.dispatch( {
+                  type: 'DECREMENT'
+                } );
 
 // I'd like to reset the count to zero
 
