6dc4fbd5dc5ac3c8cdeb52a2d2915757f7cb81a0
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-03 16:14:37 +0200

    remmeber it is a component!
    Warning: Functions are not valid as a React child. This may happen if you return a Component instead of <Component /> from render. Or maybe you meant to call this function rather than return it.

--- src/app.js
+++ src/app.js
@@ -1,86 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my dashboard component
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is my add expense component
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is my edit expense component
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    This is the help component
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404! <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    
-    <NavLink to="/" activeClassName={'is-active'} exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName={'is-active'}>Create</NavLink>
-    <NavLink to="/edit" activeClassName={'is-active'}>Edit</NavLink>
-    <NavLink to="/help" activeClassName={'is-active'}>Help</NavLink>
-    <NavLink to="/404" activeClassName={'is-active'}>404</NavLink>
-  
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      
-      <Header/>
-      
-      <Switch>
-        <Route
-          path={'/'}
-          component={ExpenseDashboardPage}
-          exact={true}
-        />
-        <Route
-          path={'/create'}
-          component={AddExpensePage}
-        />
-        <Route
-          path={'/edit'}
-          component={EditExpensePage}
-        />
-        <Route
-          path={'/help'}
-          component={HelpPage}
-        />
-        <Route
-          component={NotFoundPage}
-        />
-      </Switch>
-    
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render( routes, document.getElementById( 'app' ) );
+ReactDOM.render( <AppRouter/>, document.getElementById( 'app' ) );
