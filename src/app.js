762336acab26bcb4dbf7e9060092def93261d775
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-05 13:40:06 +0200

    default store!

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 
 import AppRouter from './routers/AppRouter';
 
+import configureStore from './store/configureStore';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-console.clear();
+
+const store = configureStore();
+
+console.log( store.getState() );
 
 ReactDOM.render( <AppRouter/>, document.getElementById( 'app' ) );
