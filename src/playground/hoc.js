f912ac274c77ad55c9be5c9dafdd66e4eae9aafd
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-05 14:07:53 +0200

    AdminInfo

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,3 +1,9 @@
+// Higher Order Component (HOC) - A component (HOC) that renders another component
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -9,4 +15,29 @@ const Info = ( props ) => (
   </div>
 );
 
-ReactDOM.render( <Info info={'These are the details'}/>, document.getElementById( 'app' ) );
+const withAdminWarning = ( WrappedComponent ) => {
+  return ( props ) => (
+    <div>
+      {props.isAdmin && <p>This is private info. Please don't share!</p>}
+      <WrappedComponent {...props} />
+    </div>
+  );
+};
+
+const requireAuthentication = ( WrappedComponent ) => {
+  return ( props ) => (
+    <div>
+      {props.isAuthenticated ? (
+        <WrappedComponent {...props} />
+      ) : (
+        <p>Please login to view the info</p>
+      )}
+    </div>
+  );
+};
+
+const AdminInfo = withAdminWarning( Info );
+const AuthInfo = requireAuthentication( Info );
+
+ReactDOM.render( <AdminInfo isAdmin={true} info="There are the details"/>, document.getElementById( 'app' ) );
+// ReactDOM.render( <AuthInfo isAuthenticated={true} info="There are the details"/>, document.getElementById( 'app' ) );
