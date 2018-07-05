35c72cdd96033a1a9e613f0d21d4ba78988da0f4
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-05 18:26:12 +0200

    Answer... because you are an indiot Ingram

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -10,5 +10,12 @@ export default ( expenses, { text, sortBy, startDate, endDate } ) => {
     const textMatch = expense.description.toLocaleLowerCase().includes( text.toLocaleLowerCase() );
     
     return startDateMatch && endDateMatch && textMatch;
+  } ).sort( ( a, b ) => {
+    if ( sortBy === 'date' ) {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    }
+    else if ( sortBy === 'amount' ) {
+      return a.amount < b.amount ? 1 : -1;
+    }
   } );
 };
