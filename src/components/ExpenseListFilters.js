9e6de9ae584d3449146afd21102d68c7eaed627c
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-05 18:36:31 +0200

    sorted!

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 
 const ExpenseListFilters = ( props ) => (
@@ -13,6 +13,29 @@ const ExpenseListFilters = ( props ) => (
         props.dispatch( setTextFilter( event.target.value ) );
       }}
     />
+    
+    <select
+      value={props.filters.sortBy}
+      onChange={( event ) => {
+        console.log( event.target.value );
+        
+        const selectedValue = event.target.value;
+        
+        switch ( selectedValue ) {
+          case 'amount':
+            return props.dispatch( sortByAmount() );
+          
+          case 'date':
+            return props.dispatch( sortByDate() );
+          
+          default:
+            console.log( 'ERROR: No such option' );
+        }
+      }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 );
 
