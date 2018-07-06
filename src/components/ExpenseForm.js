2858fec3cfc9a8120e16ef4e431f67babda36a7c
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-06 18:19:56 +0200

    moment() is our friend :)

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -65,8 +65,8 @@ export default class ExpenseForm extends Component {
     this.state = {
       description: props.expense ? props.expense.description : '',
       note: props.expense ? props.expense.note : '',
-      amount: '',
-      createdAt: moment(),
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment( props.expense.createdAt ) : moment(),
       calendarFocused: false,
       error: ''
     };
