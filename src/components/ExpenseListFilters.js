ccfc1222c75aa4a53457ddd1ac5b7df1deab8d09
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-06 20:03:32 +0200

    change to a class

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,43 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
+
+import { setStartDate, setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 
-const ExpenseListFilters = ( props ) => (
-  <div>
-    <input
-      type="text"
-      value={props.filters.text}
-      onChange={( event ) => {
-        // console.log( event.target.value );
-        props.dispatch( setTextFilter( event.target.value ) );
-      }}
-    />
+class ExpenseListFilters extends Component {
+  
+  state = {
+    calendarFocused: null
+  };
+  
+  onDatesChange = ( { startDate, endDate } ) => {
+    console.log( 'onDatesChange' );
     
-    <select
-      value={props.filters.sortBy}
-      onChange={( event ) => {
-        // console.log( event.target.value );
+    this.props.dispatch( setStartDate( { startDate } ) );
+    this.props.dispatch( setEndDate( { endDate } ) );
+  };
+  
+  onFocusChange = ( { focused } ) => {
+    
+    this.setState( () => ({ calendarFocused: focused }) );
+  };
+  
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.props.filters.text}
+          onChange={( event ) => {
+            // console.log( event.target.value );
+            this.props.dispatch( setTextFilter( event.target.value ) );
+          }}
+        />
         
-        const selectedValue = event.target.value;
+        <select
+          value={this.props.filters.sortBy}
+          onChange={( event ) => {
+            // console.log( event.target.value );
+            
+            const selectedValue = event.target.value;
+            
+            switch ( selectedValue ) {
+              case 'amount':
+                return this.props.dispatch( sortByAmount() );
+              
+              case 'date':
+                return this.props.dispatch( sortByDate() );
+              
+              default:
+                console.log( 'ERROR: No such option' );
+            }
+          }}
+        >
+          <option value="date">Date</option>
+          <option value="amount">Amount</option>
+        </select>
         
-        switch ( selectedValue ) {
-          case 'amount':
-            return props.dispatch( sortByAmount() );
-          
-          case 'date':
-            return props.dispatch( sortByDate() );
-          
-          default:
-            console.log( 'ERROR: No such option' );
-        }
-      }}
-    >
-      <option value="date">Date</option>
-      <option value="amount">Amount</option>
-    </select>
-  </div>
-);
+        <DateRangePicker
+          startDate={this.props.filters.startDate}
+          endDate={this.props.filters.endDate}
+          onDatesChange={this.onDatesChange}
+          focusedInput={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          // numberOfMonths={1}
+          // isOutsideRange={() => false}
+        />
+      
+      </div>
+    );
+  }
+}
+
 
 const mapStateToProps = ( state ) => {
   return {
