910195db6b47027cc2de9a159bb61a9ccd5f9149
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-07 20:24:10 +0200

    start and end data need to be moment instances, but that causes an issue

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -4,6 +4,58 @@ import selectExpenses from '../../selectors/expenses';
 
 console.log( moment() );
 
+const expenses = [{
+  id: 1,
+  description: 'Gum',
+  note: '',
+  amount: 195,
+  createdAt: 0
+}, {
+  id: 2,
+  description: 'Rent',
+  note: '',
+  amount: 109500,
+  createdAt: -1000
+}, {
+  id: 3,
+  description: 'Credit Card',
+  note: '',
+  amount: 66600,
+  createdAt: 1000
+}];
+
 test( 'should filter by text value', () => {
-  const result = selectExpenses();
+  
+  const filters = {
+    text: 'e',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+  
+  const result = selectExpenses( expenses, filters );
+  
+  expect( result ).toEqual( [
+                              expenses[2],
+                              expenses[3]
+                            ] );
+  
+} );
+
+test( 'should filter by startDate', () => {
+  
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+  
+  const result = selectExpenses( expenses, filters );
+  
+  expect( result ).toEqual( [
+                              expenses[2],
+                              expenses[3]
+                            ] );
+  
 } );
