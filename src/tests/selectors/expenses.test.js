3c1e911db332c398e36fee5bc0778198e0191d2f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-07 19:02:19 +0200

    we now need some test data

---
+++ src/tests/selectors/expenses.test.js
@@ -0,0 +1,9 @@
+import moment from 'moment';
+import selectExpenses from '../../selectors/expenses';
+
+
+console.log( moment() );
+
+test( 'should filter by text value', () => {
+  const result = selectExpenses();
+} );
