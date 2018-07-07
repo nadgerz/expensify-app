0db231815538b9d9130f3af6f40aead001a1a92c
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-07 18:06:45 +0200

    pays to do these tests as one goes along

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -2,11 +2,11 @@ import { setEndDate, setStartDate, setTextFilter, sortByAmount, sortByDate } fro
 
 
 test( 'should setup set start date filter action object', () => {
-  const action = setStartDate( { id: '123abc' } );
+  const action = setStartDate( { startDate: 666 } );
   
   expect( action ).toEqual( {
-                              type: 'REMOVE_EXPENSE',
-                              id: '123abc'
+                              type: 'SET_START_DATE',
+                              startDate: 666
                             } );
 } );
 
@@ -14,43 +14,43 @@ test( 'should setup set start date filter action object with defaults', () => {
   const action = setStartDate();
   
   expect( action ).toEqual( {
-                              type: 'REMOVE_EXPENSE',
-                              id: '123abc'
+                              type: 'SET_START_DATE',
+                              startDate: 666
                             } );
 } );
 
-test( 'should setup set start date filter action object', () => {
-  const action = setTextFilter( { id: '123abc' } );
+test( 'should setup set text filter action object', () => {
+  const action = setTextFilter( { text: 'bill' } );
   
   expect( action ).toEqual( {
-                              type: 'REMOVE_EXPENSE',
-                              id: '123abc'
+                              type: 'SET_TEXT_FILTER',
+                              text: 'bill'
                             } );
 } );
 
-test( 'should setup set start date filter action object', () => {
-  const action = setEndDate( { id: '123abc' } );
+test( 'should setup set end date filter action object', () => {
+  const action = setEndDate( { endDate: 123 } );
   
   expect( action ).toEqual( {
-                              type: 'REMOVE_EXPENSE',
-                              id: '123abc'
+                              type: 'SET_END_DATE',
+                              endDate: 123
                             } );
 } );
 
-test( 'should setup set start date filter action object', () => {
-  const action = sortByAmount( { id: '123abc' } );
+test( 'should setup set sort by amount filter action object', () => {
+  const action = sortByAmount( { sortBy: 'amount' } );
   
   expect( action ).toEqual( {
-                              type: 'REMOVE_EXPENSE',
-                              id: '123abc'
+                              type: 'SORT_BY_AMOUNT',
+                              sortBy: 'amount'
                             } );
 } );
 
-test( 'should setup set start date filter action object', () => {
-  const action = sortByDate( { id: '123abc' } );
+test( 'should setup set sort by date filter action object', () => {
+  const action = sortByDate( {} );
   
   expect( action ).toEqual( {
-                              type: 'REMOVE_EXPENSE',
-                              id: '123abc'
+                              type: 'SORT_BY_DATE',
+                              sortBy: 'date'
                             } );
 } );
