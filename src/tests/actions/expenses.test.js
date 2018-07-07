c42ab949e189c8904f751441647c34be888711d0
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-07 17:13:48 +0200

    set up Live Templates for tests

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,11 @@
+import { removeExpense } from '../../actions/expenses';
+
+
+test( 'should setup remove expense action object', () => {
+  const action = removeExpense( { id: '123abc' } );
+  
+  expect( action ).toBe( {
+                           type: 'REMOVE_EXPENSE',
+                           id: '123abc'
+                         } );
+} );
