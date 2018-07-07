175b45900923cd81c37434fc2847a6eab523556c
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-07 17:24:10 +0200

    These could get comples REALLY quickly

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,11 +1,33 @@
-import { removeExpense } from '../../actions/expenses';
+import { editExpense, removeExpense } from '../../actions/expenses';
 
 
 test( 'should setup remove expense action object', () => {
   const action = removeExpense( { id: '123abc' } );
   
-  expect( action ).toBe( {
-                           type: 'REMOVE_EXPENSE',
-                           id: '123abc'
-                         } );
+  expect( action ).toEqual( {
+                              type: 'REMOVE_EXPENSE',
+                              id: '123abc'
+                            } );
+} );
+
+test( 'should setup edit expense action object', () => {
+  const action = editExpense(
+    '123abc',
+    {
+      description: 'My Description',
+      note: 'My Note',
+      amount: 666,
+      createdAt: null
+    } );
+  
+  expect( action ).toEqual( {
+                              type: 'EDIT_EXPENSE',
+                              id: '123abc',
+                              updates: {
+                                description: 'My Description',
+                                note: 'My Note',
+                                amount: 666,
+                                createdAt: null
+                              }
+                            } );
 } );
