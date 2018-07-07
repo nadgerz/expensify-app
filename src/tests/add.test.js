cfeadff5c2b7d12d394f2a3eec4c5973e2809ae5
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-07 17:03:40 +0200

    simple test for defaults

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,5 +1,5 @@
 const add = ( a, b ) => a + b;
-const generateGreeting = ( name ) => `Hello ${name}!`;
+const generateGreeting = ( name = 'Anonymous' ) => `Hello ${name}!`;
 
 test( 'should add two numbers', () => {
   const result = add( 3, 4 );
@@ -11,4 +11,10 @@ test( 'should display correct greeting', () => {
   const result = generateGreeting( 'Steve' );
   
   expect( result ).toBe( 'Hello Steve!' );
+} );
+
+test( 'should display correct greeting for no name', () => {
+  const result = generateGreeting();
+  
+  expect( result ).toBe( 'Hello Anonymous!' );
 } );
