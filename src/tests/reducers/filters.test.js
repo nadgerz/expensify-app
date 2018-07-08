34d1434ef7a04e08bb3cbfc396fb262163ecaad6
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-08 16:05:40 +0200

    for the sort by date, we need to set up state to be different first - set to 'amount'

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -22,7 +22,16 @@ test( 'should set sortBy to amount', () => {
 } );
 
 test( 'should set sortBy to date', () => {
-  const state = filtersReducer( undefined, { type: 'SORT_BY_DATE' } );
+  
+  const currentState = {
+    text: '',
+    startDate: undefined,
+    endDate: undefined,
+    sortBy: 'amount'
+  };
+  const action = { type: 'SORT_BY_DATE' };
+  
+  const state = filtersReducer( currentState, action );
   
   expect( state.sortBy ).toBe( 'date' );
   
