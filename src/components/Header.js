3e6359e1c691e174d4f435371d54fc159360ebde
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 14:35:36 +0200

    match snapshots

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
     <NavLink to="/create" activeClassName={'is-active'}>Create</NavLink>
     {/*<NavLink to="/edit" activeClassName={'is-active'}>Edit</NavLink>*/}
     <NavLink to="/help" activeClassName={'is-active'}>Help</NavLink>
-    <NavLink to="/404" activeClassName={'is-active'}>404</NavLink>
+    {/*<NavLink to="/404" activeClassName={'is-active'}>404</NavLink>*/}
   
   </header>
 );
