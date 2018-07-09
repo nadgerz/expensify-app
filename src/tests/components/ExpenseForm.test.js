aece29dfff2628e043b9cd533b2e0cbb474d381d
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 17:48:10 +0200

    textarea test!

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -29,3 +29,37 @@ test( 'should render error for invalid form submission', () => {
   expect( wrapper.state( 'error' ).length ).toBeGreaterThan( 0 );
   expect( wrapper ).toMatchSnapshot();
 } );
+
+test( 'should set description on input change', () => {
+  // 1: Render ExpenseForm.
+  const wrapper = shallow( <ExpenseForm/> );
+  
+  // 2: Change the input.
+  const value = 'New description';
+  
+  wrapper.find( 'input' ).at( 0 ).simulate( 'change', {
+    target: {
+      value
+    }
+  } );
+  
+  // 3: Make an assertion that the description state was set.
+  expect( wrapper.state( 'description' ) ).toBe( value );
+} );
+
+test( 'should set not on textarea change', () => {
+  // 1: Render ExpenseForm.
+  const wrapper = shallow( <ExpenseForm/> );
+  
+  // 2: Change the textarea.
+  const value = 'New note';
+  
+  wrapper.find( 'textarea' ).simulate( 'change', {
+    target: {
+      value
+    }
+  } );
+  
+  // 3: Make an assertion that the note state was set.
+  expect( wrapper.state( 'note' ) ).toBe( value );
+} );
