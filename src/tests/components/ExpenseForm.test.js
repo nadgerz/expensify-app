de37c208db283635fe4d805b535b8a894463b3e7
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 16:43:12 +0200

    date is changing, so we fail

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 
 
-test( 'should render ExpenseForm', () => {
+test( 'should render ExpenseForm correctly', () => {
   const wrapper = shallow( <ExpenseForm/> );
   
   expect( wrapper ).toMatchSnapshot();
