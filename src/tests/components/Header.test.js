5435766ffcb9cd62b473b1de9a39d58aec146235
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 14:56:29 +0200

    better API

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 
 test( 'should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render( <Header/> );
+  const wrapper = shallow( <Header/> );
   
-  // console.log( renderer.getRenderOutput() );
-  expect( renderer.getRenderOutput() ).toMatchSnapshot();
+  // expect( wrapper.find( 'h1' ).length ).toBe( 1 );
+  // expect( wrapper.find( 'h1' ).text() ).toBe( 'Expensify' );
 } );
