d377fa7f198dce65ff6ff8a9495e7b6af6f804cd
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 16:41:29 +0200

    should render ExpenseForm correctly

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ExpenseForm from '../../components/ExpenseForm';
+
+
+test( 'should render ExpenseForm', () => {
+  const wrapper = shallow( <ExpenseForm/> );
+  
+  expect( wrapper ).toMatchSnapshot();
+} );
