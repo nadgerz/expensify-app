bc23ac5fae95ac74569c53de686285481cb13a75
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 15:07:10 +0200

    Automate enzyme-to-json

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-
 import Header from '../../components/Header';
-
+// import toJson from 'enzyme-to-json';
 
 test( 'should render Header correctly', () => {
   const wrapper = shallow( <Header/> );
   
   // expect( wrapper.find( 'h1' ).length ).toBe( 1 );
   // expect( wrapper.find( 'h1' ).text() ).toBe( 'Expensify' );
-  expect( toJson( wrapper ) ).toMatchSnapshot();
+  expect( wrapper ).toMatchSnapshot();
 } );
