b42320f79932172d1a7bc08a9258ccec9e0bf71f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-09 15:19:51 +0200

    created tst2 to speed this shit up

---
+++ src/tests/components/ExpenseList.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { ExpenseList } from '../../components/ExpenseList';
+import expenses from '../fixtures/expenses';
+
+
+test( 'should render ExpenseList', () => {
+  const wrapper = shallow( <ExpenseList expenses={expenses}/> );
+  
+  expect( wrapper ).toMatchSnapshot();
+} );
