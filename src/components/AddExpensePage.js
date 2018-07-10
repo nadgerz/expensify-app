04e1628e592618ec453e244576e2cf45db3fdef6
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-10 14:41:17 +0200

    switch to class component - why?

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { addExpense } from '../actions/expenses';
 import ExpenseForm from './ExpenseForm';
 
 
-const AddExpensePage = ( props ) => (
-  <div>
-    <h1>Add Expense</h1>
-    
-    <ExpenseForm
-      onSubmit={( expense ) => {
-        // console.log( 'ADD!' );
-        // console.log( expense );
+class AddExpensePage extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
         
-        // props.dispatch( addExpense( expense ) );
-        props.onSubmit( expense );
-        
-        props.history.push( '/' );
-      }}
-    />
-  </div>
-);
+        <ExpenseForm
+          onSubmit={( expense ) => {
+            this.props.onSubmit( expense );
+            
+            this.props.history.push( '/' );
+          }}
+        />
+      </div>
+    );
+  }
+}
+
 
 const mapDispatchToProps = ( dispatch ) => ({ onSubmit: ( expense ) => dispatch( addExpense( expense ) ) });
 
