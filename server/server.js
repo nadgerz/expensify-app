292785123118d68e758436c3e92a0971793aafaf
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-10 20:26:40 +0200

    sendFile or send?

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const publicPath = path.join( __dirname, '../public' );
 
 app.use( express.static( publicPath ) );
 
+app.get( '*', ( req, res ) => {
+  res.sendFile( path.join( __dirname, '../public/index.html' ) );
+} );
+
 const port = 3000;
 
 app.listen( port, () => {
