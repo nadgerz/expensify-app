d80731a7e2c444ad675d123c580c9f6a0426af7b
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-10 20:28:15 +0200

    the app will work regardless of what page the user starts on

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const publicPath = path.join( __dirname, '../public' );
 app.use( express.static( publicPath ) );
 
 app.get( '*', ( req, res ) => {
-  res.sendFile( path.join( __dirname, '../public/index.html' ) );
+  res.sendFile( path.join( publicPath, 'index.html' ) );
 } );
 
 const port = 3000;
