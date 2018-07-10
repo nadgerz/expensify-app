edcf24c7f6d4b59fb386103f7a5d5ddda6def194
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-10 14:55:45 +0200

    this is going to need some checking out

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { AddExpensePage } from '../../components/AddExpensePage';
+import expenses from '../fixtures/expenses';
 
 
 test( 'should render AddExpensePage correctly', () => {
@@ -11,4 +12,17 @@ test( 'should render AddExpensePage correctly', () => {
   const wrapper = shallow( <AddExpensePage onSubmit={onSubmit} history={history}/> );
   
   expect( wrapper ).toMatchSnapshot();
+} );
+
+test( 'should handle onSubmit', () => {
+  const onSubmit = jest.fn();
+  const history = { push: jest.fn() };
+  
+  const wrapper = shallow( <AddExpensePage onSubmit={onSubmit} history={history}/> );
+  
+  const expense = expenses[1];
+  wrapper.find( 'ExpenseForm' ).prop( 'onSubmit' )( expense );
+  
+  expect( history.push ).toHaveBeenLastCalledWith( '/' );
+  expect( onSubmit ).toHaveBeenLastCalledWith( expense );
 } );
