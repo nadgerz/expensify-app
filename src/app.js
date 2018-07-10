69d916dcc33934521b7c1eec879bbe359869fd78
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-10 18:12:08 +0200

    save from being removed

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ import AppRouter from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
 
+/*
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 
 import getVisibleExpenses from './selectors/expenses';
+*/
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
