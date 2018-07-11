0c31f401f977cdec4945a6364feb9741fadbcbe9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-11 14:12:38 +0200

    Alternative

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,11 +1,23 @@
+/*
 export default ( expenses = [] ) => {
-  
-  // const reducer = ( accumulator, currentValue ) => accumulator + currentValue;
   
   return expenses.reduce( ( accumulator, expense ) => {
     const amount = expense.amount;
     
     return accumulator + amount;
   }, 0 );
+}
+*/
+
+export default ( expenses ) => {
+  
+  if ( expenses.length === 0 ) {
+    return 0;
+  }
+  else {
+    const reducer = ( accumulator, expense ) => accumulator + expense.amount;
+    
+    return expenses.reduce( reducer, 0 );
+  }
 }
 
