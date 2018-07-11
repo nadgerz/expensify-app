0e53efce7dd79934e84328ec2971f05dbea8ea3a
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-11 19:10:19 +0200

    You can do as many things as you like

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -16,7 +16,11 @@ const promise = new Promise( ( resolve, reject ) => {
 console.log( 'before' );
 
 promise.then( ( data ) => {
-  console.log( data );
+  console.log( '1', data );
+} );
+
+promise.then( ( data ) => {
+  console.log( '2', data );
 } );
 
 console.log( 'after' );
