744b982eebf3b6317646487113882191cfcc62d5
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-11 19:14:38 +0200

    resolved OR rejected!

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,26 +1,25 @@
 const promise = new Promise( ( resolve, reject ) => {
   
-  console.log( 'Starting a wait of 1.5 seconds' );
+  const millis = 5000;
+  
+  console.log( `Starting a wait of ${millis / 1000} seconds` );
   
   setTimeout( () => {
-    resolve( 'This got resolved after 1.5 seconds' );
-  }, 5000 );
+    resolve( `This got resolved after ${millis / 1000} seconds.` );
+    resolve( 'This is my other resolved data.' );
+  }, millis );
   
-/*
-  setTimeout( () => {
-    console.log( 'This waited 1.5 seconds' );
-  }, 5000 );
-*/
+  /*
+    setTimeout( () => {
+      console.log( `This waited ${millis/1000} seconds` );
+    }, millis );
+  */
 } );
 
 console.log( 'before' );
 
 promise.then( ( data ) => {
   console.log( '1', data );
-} );
-
-promise.then( ( data ) => {
-  console.log( '2', data );
 } );
 
 console.log( 'after' );
