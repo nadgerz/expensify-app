622f658f09426d67cc327fa5e0eeedbc17e99ad8
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-13 18:03:09 +0200

    Async needs promise chaining... need to study this all quite a bit more!

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,13 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { addExpense, editExpense, removeExpense, startAddExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 
+
+const createMockStore = configureMockStore( [
+                                              thunk
+                                            ] );
 
 test( 'should setup remove expense action object', () => {
   const action = removeExpense( { id: '123abc' } );
@@ -57,7 +64,19 @@ test( 'should setup add expense action object with provided values', () => {
 } );
 
 test( 'should add expense to database and store', () => {
-
+  //  Section 15: Lecture 153. Testing Async Redux Actions: Part I 16:59
+  const store = createMockStore( {} );
+  
+  const expenseData = {
+    description: '',
+    note: '',
+    amount: 0,
+    createdAt: 0
+  };
+  
+  const expense = expenses[1];
+  
+  store.dispatch( startAddExpense( expense ) );
 } );
 
 test( 'should add expense with defaults to database and store', () => {
