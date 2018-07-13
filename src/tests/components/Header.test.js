2e3da70c32358d65be666c4768a13b9f68401df4
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-13 18:47:41 +0200

    cleanup

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header';
-// import toJson from 'enzyme-to-json';
 
 test( 'should render Header correctly', () => {
   const wrapper = shallow( <Header/> );
   
-  // expect( wrapper.find( 'h1' ).length ).toBe( 1 );
-  // expect( wrapper.find( 'h1' ).text() ).toBe( 'Expensify' );
   expect( wrapper ).toMatchSnapshot();
 } );
