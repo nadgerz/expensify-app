e10e5926757338e1209db0920b3c5e72cfd97bba
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-14 01:26:42 +0200

    how to do this?

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 
-const Header = () => (
-  <header>
-    
-    <h1>Expensify</h1>
-    
-    <NavLink to="/" activeClassName={'is-active'} exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName={'is-active'}>Create</NavLink>
-    <NavLink to="/help" activeClassName={'is-active'}>Help</NavLink>
-    {/*<NavLink to="/404" activeClassName={'is-active'}>404</NavLink>*/}
-    
-    <button>Logout</button>
+const Header = () => {
   
-  </header>
-);
+  const isActiveClassName = 'activeClassName={\'is-active\'}';
+  console.log( isActiveClassName );
+  
+  return (
+    <header>
+      
+      <h1>Expensify</h1>
+      
+      <NavLink to="/" activeClassName={'is-active'} exact={true}>Dashboard</NavLink>
+      <NavLink to="/create" activeClassName={'is-active'}>Create</NavLink>
+      <NavLink to="/help" activeClassName={'is-active'}>Help</NavLink>
+      {/*<NavLink to="/404" activeClassName={'is-active'}>404</NavLink>*/}
+      
+      <button>Logout</button>
+    
+    </header>
+  );
+};
 
 export default Header;
