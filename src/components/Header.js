aca8dba3a98e955ec23476f38eb9ef6888eabf84
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-13 21:23:04 +0200

    Add logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
+    
     <h1>Expensify</h1>
     
-    <NavLink to="/" activeClassName={'is-active'} exact={true}>Login</NavLink>
-    <NavLink to="/dashboard" activeClassName={'is-active'}>Dashboard</NavLink>
+    <NavLink to="/" activeClassName={'is-active'} exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName={'is-active'}>Create</NavLink>
     <NavLink to="/help" activeClassName={'is-active'}>Help</NavLink>
     {/*<NavLink to="/404" activeClassName={'is-active'}>404</NavLink>*/}
+    
+    <button>Logout</button>
   
   </header>
 );
