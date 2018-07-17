22dd2b8f01867266ada71f1ca1fda1c59c4f267a
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-17 17:39:01 +0200

    Adding a signOut

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { startLogout } from '../actions/auth';
 
 
-const Header = () => {
+export const Header = ( { startLogout } ) => {
   
   const isActiveClassName = 'activeClassName={\'is-active\'}';
-  console.log( isActiveClassName );
+  // console.log( isActiveClassName );
   
   return (
     <header>
@@ -17,10 +20,14 @@ const Header = () => {
       <NavLink to="/help" activeClassName={'is-active'}>Help</NavLink>
       {/*<NavLink to="/404" activeClassName={'is-active'}>404</NavLink>*/}
       
-      <button>Logout</button>
+      <button onClick={startLogout}>Logout</button>
     
     </header>
   );
 };
 
-export default Header;
+const mapDispatchToProps = ( dispatch ) => ({
+  startLogout: () => dispatch( startLogout() )
+});
+
+export default connect( undefined, mapDispatchToProps )( Header );
