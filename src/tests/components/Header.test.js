006af1359f20765cd149541a05a58ebb567ced0b
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-17 17:56:04 +0200

    Dang... forgot all this spy shit :)

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import { Header } from '../../components/Header';
 
 
@@ -14,9 +15,11 @@ test( 'should call startLogout on button click', () => {
   
   const startLogout = jest.fn();
   
-  const wrapper = shallow( <Header startLogout={startLogout} /> );
+  const wrapper = shallow( <Header startLogout={startLogout}/> );
   
-  wrapper.find('button').simulate('click')
+  wrapper.find( 'button' ).simulate( 'click' );
+  
+  expect( startLogout ).toHaveBeenCalled();
 } );
 
 // LoginPage test file -> should call startLogin on button click
