bdd4fe5667e27fcb01375707b0a20ccf76140099
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-18 17:51:59 +0200

    Need to check out what these reducers do

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
-import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -13,7 +14,8 @@ export default () => {
   return createStore(
     combineReducers( {
                        expenses: expensesReducer,
-                       filters: filtersReducer
+                       filters: filtersReducer,
+                       auth: authReducer
                      } ), /* preloadedState, */
     composeEnhancers( applyMiddleware( thunk ) )
   );
