57cd6117af67958a21c0ed6cbee8cfcc9a85116e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-07-18 18:06:56 +0200

    Adding some missing options

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
 import AppRouter, { history } from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
 
 import { firebase } from './firebase/firebase';
 
